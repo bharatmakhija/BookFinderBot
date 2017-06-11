b1acc8e62df672cafbaa2416f6e456ee78ed3791
bharat <bharatkumarmakhija14@gmail.com>
2017-06-11 19:57:39 +0530

    jlkj

---
+++ StringConstants.py
@@ -0,0 +1,14 @@
+welcomeMsg = "hi, this is my first python script, this is all about you giving booktitle name and extracting every information related to it on your screen"
+timeoutError = "TimeOut error"
+badUrlError = "Url is bad: TooManyRedirects"
+unexpectedError = "Some Unexpected error has occured::"
+enterSerialNoMsg = 'enter books serial number which you want to review: '
+goodReadSearchString = "https://www.goodreads.com/search?page="
+goodReadsString = "https://www.goodreads.com"
+continueString = 'enter 1 to continue , 0 to end: '
+exitString = 'enter key to exit: '
+loadingMsg = 'keep loading 1/0 :'
+noInformationFoundError = "No Information Found!"
+informationLoadingMessage = "PLease Wait! Loading Information on "
+havingUrlMsg= " haiving url: "
+enterBookTitleMsg = 'Enter Book title: '
